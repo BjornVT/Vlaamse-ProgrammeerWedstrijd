bda7b5b952e8bcd04ed5dc1876af856c010b4afa
Bjorngulf <bjorn.vantilt@gmail.com>
2016-02-18 12:38:54 +0100

    Array vergelijk functie toegevoegd

--- categorie4/schuifPuzzel/schuifpuzzel.php
+++ categorie4/schuifPuzzel/schuifpuzzel.php
@@ -1,4 +1,5 @@
 <?php
+	define("MAXDIEPTE", 14);
 	$n = intval(trim(fgets(STDIN)));
 
 	for($i=1; $i<=$n; $i++){
@@ -11,11 +12,12 @@
 		for($j=0; $j<$r; $j++){
 			for($l=0; $l<$k; $l++){
 				$puzzel[$j][$l] = $s[$j*$k+$l];
+				if($s[$j*$k+$l] == ' '){
+					$leegr = $j;
+					$leegk = $l;
+				}
 			}
 		}
-
-		//print_r($puzzel);
-
 		$opl = array();
 		$s = trim(fgets(STDIN));
 		for($j=0; $j<$r; $j++){
@@ -23,7 +25,27 @@
 				$opl[$j][$l] = $s[$j*$k+$l];
 			}
 		}
-		print_r($opl);
-
+	}
+/*
+	$nMoves = 0;
+	function doMove($diepte, 
+	{
+		if($diepte<MAXDIEPTE){
+			//Max diepte nog niet bereikt
+			if(
+		}
+		return -1;
+	}
+ */
+	function cmpArray($a1, $a2, $r, $k)
+	{
+		for($i=0; $i<$r; $i++){
+			for($j=0; $j<$k; $j++){
+				if($a1[$i][$j] != $a2[$i][$j]){
+					return -1;
+				}
+			}
+		}
+		return 0;
 	}
 ?>
