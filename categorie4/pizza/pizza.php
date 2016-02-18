487c0be4afd095f07d3a498a27e51742cb303585
Bjorngulf <bjorn.vantilt@gmail.com>
2016-02-18 15:00:53 +0100

    invoer binnenlezen

---
+++ categorie4/pizza/pizza.php
@@ -0,0 +1,23 @@
+<?php
+	$n = intval(trim(fgets(STDIN)));
+
+	for($i=1; $i<=$n; $i++){
+		$s = explode(' ', trim(fgets(STDIN)));
+		$ndelen = $s[0];
+		$delen = array();
+		for($j=0; $j<$ndelen; $j++){
+			$delen[$j] = $s[$j+1];
+		}
+
+
+
+
+
+
+
+
+
+
+
+	}
+?>
