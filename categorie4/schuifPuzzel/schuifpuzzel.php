4a79290502af5878cfd30261619ef9ba391fe9ba
Bjorngulf <bjorn.vantilt@gmail.com>
2016-02-18 14:39:08 +0100

    Werkende oplossing

--- categorie4/schuifPuzzel/schuifpuzzel.php
+++ categorie4/schuifPuzzel/schuifpuzzel.php
@@ -8,7 +8,7 @@
 		$k = $s[1];
 
 		$puzzel = array();
-		$s = trim(fgets(STDIN));
+		$s = fgets(STDIN);
 		for($j=0; $j<$r; $j++){
 			for($l=0; $l<$k; $l++){
 				$puzzel[$j][$l] = $s[$j*$k+$l];
@@ -19,18 +19,17 @@
 			}
 		}
 		$opl = array();
-		$s = trim(fgets(STDIN));
+		$s = fgets(STDIN);
 		for($j=0; $j<$r; $j++){
 			for($l=0; $l<$k; $l++){
 				$opl[$j][$l] = $s[$j*$k+$l];
 			}
 		}
 
-		//print_r($puzzel);
-
 		$minMove = MAXDIEPTE;
 		doMove(0, $puzzel, $leegr, $leegk, -1);
-		echo $minMove;
+		printf("%d\n", $minMove);
+
 	}
 
 	// 0 down
@@ -39,13 +38,11 @@
 	// 3 rechts
 	function doMove($diepte, $toestand, $leegr, $leegk, $richting)
 	{
-		if($diepte<MAXDIEPTE){
+		if(($diepte <= MAXDIEPTE) && ($diepte < $GLOBALS['minMove'])){
 			//Max diepte nog niet bereikt
-			//print_r($toestand);
-			printf("diepte: %d\n", $diepte);
 			if(cmpArray($toestand, $GLOBALS['opl'], $GLOBALS['r'], $GLOBALS['k']) == 0)
 			{
-				echo "gevonden\n";
+				//Een oplossing gevonden
 				$GLOBALS['minMove'] = $GLOBALS['minMove'] > $diepte ? $diepte : $GLOBALS['minMove'];
 				return;
 			}
@@ -54,30 +51,31 @@
 				//We kunnen naar boven schuiven
 				$temp = $toestand;
 				$temp[$leegr][$leegk] = $toestand[$leegr-1][$leegk];
-				$temp[$leegr-1][$leegk] = ' ';
+				$temp[$leegr-1][$leegk] = chr(0x20);
 				doMove($diepte+1, $temp, $leegr-1, $leegk, 1);
 			}
 			if(($leegr < ($GLOBALS['r']-1)) && ($richting != 1)){
 				//Naar beneden schuiven die handel
 				$temp = $toestand;
 				$temp[$leegr][$leegk] = $toestand[$leegr+1][$leegk];
-				$temp[$leegr+1][$leegk] = ' ';
+				$temp[$leegr+1][$leegk] = chr(0x20);
 				doMove($diepte+1, $temp, $leegr+1, $leegk, 0);
 			}
 			if(($leegk > 0) && ($richting != 3)){
 				//Naar links schuiven
 				$temp = $toestand;
 				$temp[$leegr][$leegk] = $toestand[$leegr][$leegk-1];
-				$temp[$leegr][$leegk-1] = ' ';
+				$temp[$leegr][$leegk-1] = chr(0x20);
 				doMove($diepte+1, $temp, $leegr, $leegk-1, 2);
 			}
 			if(($leegk < ($GLOBALS['k']-1)) && ($richting != 2)){
 				//Naar rechts schuiven
 				$temp = $toestand;
 				$temp[$leegr][$leegk] = $toestand[$leegr][$leegk+1];
-				$temp[$leegr][$leegk+1] = ' ';
+				$temp[$leegr][$leegk+1] = chr(0x20);
 				doMove($diepte+1, $temp, $leegr, $leegk+1, 3);
 			}
+		 
 		}
 	}
 
@@ -85,7 +83,7 @@
 	{
 		for($i=0; $i<$r; $i++){
 			for($j=0; $j<$k; $j++){
-				if($a1[$i][$j] != $a2[$i][$j]){
+				if(!($a1[$i][$j] == $a2[$i][$j])){
 					return -1;
 				}
 			}
