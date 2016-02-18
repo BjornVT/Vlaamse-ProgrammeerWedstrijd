7d51402841280533952d23e5f89b46dbbf2385b2
Bjorngulf <bjorn.vantilt@gmail.com>
2016-02-18 15:18:14 +0100

    Opkuis commentaar

--- categorie4/schuifPuzzel/schuifpuzzel.php
+++ categorie4/schuifPuzzel/schuifpuzzel.php
@@ -1,12 +1,15 @@
 <?php
 	define("MAXDIEPTE", 14);
+	//Aantal testgevallen inlezen
 	$n = intval(trim(fgets(STDIN)));
 
 	for($i=1; $i<=$n; $i++){
+		//Het aantal rijen en kol binnenlezen
 		$s = explode(' ', trim(fgets(STDIN)));
 		$r = $s[0];
 		$k = $s[1];
 
+		//Puzzel inlezen
 		$puzzel = array();
 		$s = fgets(STDIN);
 		for($j=0; $j<$r; $j++){
@@ -18,6 +21,7 @@
 				}
 			}
 		}
+		//De te bekomen oplossing inlezen
 		$opl = array();
 		$s = fgets(STDIN);
 		for($j=0; $j<$r; $j++){
@@ -26,18 +30,34 @@
 			}
 		}
 
+		//Globale var juist zetten voor de volgende functie.
 		$minMove = MAXDIEPTE;
 		doMove(0, $puzzel, $leegr, $leegk, -1);
+
+		//Onze opl afprinten.
 		printf("%d\n", $minMove);
 
 	}
 
-	// 0 down
-	// 1 up
-	// 2 links
-	// 3 rechts
 	function doMove($diepte, $toestand, $leegr, $leegk, $richting)
 	{
+		/**
+		 * Recursieve fuctie die via een Brute-Force de oplossing gaat zoeken 
+		 * van de schuifpuzzel.
+		 *
+		 * \param $diepte Hoeveel stappen hebben we al gedaan om hier te komen
+		 * \param $toestand Huidig puzzel toestand
+		 * \param $leegr Rij waar lege plaats is
+		 * \param $leegk Kolom waar lege plaats is
+		 * \param $richting de richting die we juist genomen hebben:
+		 * 		0 Naar beneden
+		 * 		1 Naar boven
+		 * 		2 Naar links
+		 * 		3 Naar rechts
+		 * \return globale $minMove wordt aangepast met het min aantal genomen
+		 * stappen
+		 */
+
 		if(($diepte <= MAXDIEPTE) && ($diepte < $GLOBALS['minMove'])){
 			//Max diepte nog niet bereikt
 			if(cmpArray($toestand, $GLOBALS['opl'], $GLOBALS['r'], $GLOBALS['k']) == 0)
@@ -81,6 +101,15 @@
 
 	function cmpArray($a1, $a2, $r, $k)
 	{
+		/**
+		 * Functie om te kijken of twee arrays gelijk zijn.
+		 * \param $a1 Eerste array om te vergelijken
+		 * \param $a2 Tweede array om te vergelijken
+		 * \param $r Aantal rijen in de Array
+		 * \param $k Aantal kollommen in de Array
+		 * \return 0 voor gelijk -1 voor niet gelijk
+		 */
+
 		for($i=0; $i<$r; $i++){
 			for($j=0; $j<$k; $j++){
 				if(!($a1[$i][$j] == $a2[$i][$j])){
