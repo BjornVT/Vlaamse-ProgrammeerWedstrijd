639a8cd13e6f98412b1b3edc761b92d4a38c8a4b
Bjorngulf <bjorn.vantilt@gmail.com>
2016-02-20 14:28:12 +0100

    Ergens nog oneindige lus

--- categorie4/pizza/pizza.php
+++ categorie4/pizza/pizza.php
@@ -9,10 +9,85 @@
 			$delen[$j] = $s[$j+1];
 		}
 
+		$max = 0;
+		$tot = 0;
+		//Bij elk stuk eens beginnen
+		//for($j=0; $j<$ndelen; $j++){
+		$j = 0;
+			$temp = $delen;
+			$tot = $temp[$j];
+			$temp[$j] = 0;
+			eat($temp, $ndelen, $j, $tot);
+			$max = $max < $tot ? $tot : $max;
+		//}
 
 
+		printf("%d\n", $max);
 
+	}
 
+	function eat($toestand, $n, $plaats, $tot)
+	{
+		//Mogelijkheid 1
+		$temp = $toestand;
+		//Bob neemt links
+		getPiece($temp, $n, $plaats, -1);
+		//Alice neemt links
+		$ret = getPiece($temp, $n, $plaats, -1);
+		if($ret != -1){
+			$tempTot = $tot + $ret;
+			$GLOBALS['tot'] = $tempTot > $GLOBALS['tot'] ? $tempTot : $GLOBALS['tot'];
+			eat($temp, $n, $plaats, $tempTot);
+		}
+		else{
+			return;
+		}
+
+		//Mogelijkheid 2
+		$temp = $toestand;
+		//Bob neemt links
+		getPiece($temp, $n, $plaats, -1);
+		//Alice neemt rechts
+		$ret = getPiece($temp, $n, $plaats, 1);
+		if($ret != -1){
+			$tempTot = $tot + $ret;
+			$GLOBALS['tot'] = $tempTot > $GLOBALS['tot'] ? $tempTot : $GLOBALS['tot'];
+			eat($temp, $n, $plaats, $tempTot);
+		}
+		else{
+			return;
+		}
+
+		//Mogelijkheid 3
+		$temp = $toestand;
+		//Bob neemt rechts 
+		getPiece($temp, $n, $plaats, 1);
+		//Alice neemt links
+		$ret = getPiece($temp, $n, $plaats, -1);
+		if($ret != -1){
+			$tempTot = $tot + $ret;
+			$GLOBALS['tot'] = $tempTot > $GLOBALS['tot'] ? $tempTot : $GLOBALS['tot'];
+			eat($temp, $n, $plaats, $tempTot);
+		}
+		else{
+			return;
+		}
+
+		//Mogelijkheid 4 
+		$temp = $toestand;
+		//Bob neemt rechts 
+		getPiece($temp, $n, $plaats, 1);
+		//Alice neemt rechts
+		$ret = getPiece($temp, $n, $plaats, 1);
+		if($ret != -1){
+			$tempTot = $tot + $ret;
+			$GLOBALS['tot'] = $tempTot > $GLOBALS['tot'] ? $tempTot : $GLOBALS['tot'];
+			eat($temp, $n, $plaats, $tempTot);
+		}
+		else{
+			return;
+		}
+	}
 
 	function getPiece(&$toestand, $n, $plaats, $richt)
 	{
