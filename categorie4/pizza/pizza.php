3c314a45fe208201904e36f3226b7543c2bd08f1
Bjorngulf <bjorn.vantilt@gmail.com>
2016-02-20 13:43:26 +0100

    updated functie zodat geloopt wordt van 0 naar n

--- categorie4/pizza/pizza.php
+++ categorie4/pizza/pizza.php
@@ -16,13 +16,22 @@
 
 	function getPiece($toestand, $n, $plaats, $richt)
 	{
+		if(count($toestand) == 0){
+			return -1;
+		}
+
 		for($i=0; $i<$n && $i>=0; $i+=$richt){
 			if($toestand[$i] != 0){
 				$temp = $toestand[$i];
 				$toestand = 0;
 				return $temp;
 			}
+			if($i == 0){
+				$i = $n - 1;
+			}
+			else if($i == $n - 1){
+				$i = 0;
+			}
 		}
-		return -1;
 	}
 ?>
